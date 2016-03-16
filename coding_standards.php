484f8f892139b19ce46c90befa38debc50df2853
lfamlfam <lfamlfam@gmail.com>
2016-03-16 20:42:47 -0300

    Exemplo 1

---
+++ coding_standards.php
@@ -0,0 +1,14 @@
+<?php
+echo "Conding Standards";
+
+$nome = 'Luiz Fernando';
+
+$nota = 2 + 6;
+
+$nome_completo = 'Luiz Fernando '.'Albertin ';
+
+$nome_completo .= 'Bono Milan';
+
+echo '<br><br> Nome: '.$nome_completo;
+
+?>
